cf2820a6fff5608bebddcdfe06529e84fa40b5d3
Daniel <daniel.evans@ic-consult.com>
2023-07-21 06:29:59 +0100

    added utils files - Report

---
+++ src/test/java/com/ensek/utils/TestReportUtils.java
@@ -0,0 +1,13 @@
+package com.ensek.utils;
+
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+
+public class TestReportUtils {
+
+    public static void logTestStatus(ITestResult result) {
+        System.out.println("Test: " + result.getMethod().getMethodName() + " - Status: " + result.getStatus());
+    }
+
+    // Add more custom reporting methods as per your requirements
+}
